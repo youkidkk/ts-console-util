c132b1cb198196658740d1d1fc3614b05b16fc87
youkidkk <youkidkk80@gmail.com>
2021-09-26 11:33:58 +0900

    ビルド

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,4 +1,14 @@
 export declare function getTextWidth(text: string): number;
 export declare function paddingLeft(text: string, width: number, padChar?: string): string;
 export declare function paddingRight(text: string, width: number, padChar?: string): string;
+declare class ConsoleClass {
+    previousLogWidth: number;
+    constructor();
+    print(text: string): void;
+    printLine(text: string): void;
+    printError(text: string): void;
+    printErrorLine(text: string): void;
+}
+export declare const Console: ConsoleClass;
+export {};
 //# sourceMappingURL=index.d.ts.map
